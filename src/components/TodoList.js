aec1289b4bb505b79815cf8791b501dfabe289ff
rohitgalande1032 <rohitg3234@gmail.com>
2025-02-10 17:27:22 +0530

    marked as completed

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,9 +7,9 @@ const TodoList = ({ todos, handleComplete }) => {
       <h2>Todo List</h2>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>
+          <li key={todo.id} style={{textDecoration: todo.completed ? 'line-through' : 'none'}}>
             {todo.task}
-            {todo.completed ? <span> ✅ Completed</span> : (
+            {!todo.completed &&  (
             <button data-testid={`complete-btn-${todo.id}`} onClick={() => handleComplete(todo.id)}>
               Complete
             </button>
